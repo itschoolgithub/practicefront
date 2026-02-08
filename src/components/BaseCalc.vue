52e9cd0b46ac72db10cc9b3a5eb018e3e3bf9b15
itschool <itschool@tou.edu.kz>
2026-02-08 10:47:51 +0500

    init

---
+++ src/components/BaseCalc.vue
@@ -0,0 +1,36 @@
+<template>
+    <h2>Калькулятор</h2>
+    <input
+        type="number"
+        v-model="a"
+    >
+    <input
+        type="number"
+        v-model="b"
+    >
+    <button
+        @click="sendForm"
+    >Отправить</button>
+    <h2
+        v-if="result !== null"
+    >Результат: {{ result }}</h2>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        data() {
+            return {
+                a: null,
+                b: null,
+                result: null
+            }
+        },
+        methods: {
+            async sendForm() {
+                const response = await axios.get(`http://practice.be/calc.php?a=${this.a}&b=${this.b}`)
+                this.result = response.data.result;
+            }
+        }
+    }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <base-calc></base-calc>
+</template>
+
+<script>
+    import BaseCalc from '@/components/BaseCalc.vue';
+    export default {
+        components: {
+            BaseCalc
+        }
+    }
+</script>
